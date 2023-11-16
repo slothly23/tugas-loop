9d3c9cb94d9d05d27691d33f1fc7810a89f0c0c2
Nur Inayah <150806284+slothly23@users.noreply.github.com>
2023-11-17 06:35:14 +0700

    menambahkan tugas conditional

--- latihan.js
+++ latihan.js
@@ -42,9 +42,53 @@ console.log(pembagian)
 console.log(modulus)
 console.log("-------------------------------------------")
 
-
 // TUGAS CONDITIONAL
 
+// Buat conditional IF .. ELSE IF yang menggambarkan divisi dan tugas divisi pada STARTUP
+let posisi = "CMO"
+if (posisi === "CEO"){
+    console.log("CEO adalah pemimpin dan perwakilan perusahaan.")
+} 
+else if(posisi === "CTO"){
+    console.log("CTO atau chief technology officer bertanggung jawab atas produk.")
+}
+else if (posisi === "CFO"){
+  console.log("CFO bertugas membentuk strategi penggalangan dana.")
+}
+else if  (posisi === "CMO"){
+console.log("CMO mbertanggung jawab atas semua aspek pemasaran.")
+}
+else{
+console.log("Maaf posisi yang dimaksuy tidak ditemukan.")
+}
+console.log("-------------------------------------------")
+
+//Buat conditional untuk membandingkan 2 buah nilai. Tampilkan apakah salah satu nilai misal a lebih besar dari b, a lebih kecil dari b, dan a sama dengan b
+let a = 1
+let b = 5
+if (a > b){
+    console.log(a + " lebih dari " + b)
+}
+else if (a < b){
+    console.log(a + " kurang dari " + b)
+}
+else if (a == b){
+    console.log(a + " sama dengan " + b)
+}
+console.log("-------------------------------------------")
+
+//Gunakan switch case untuk mengetahui nama hari berdasarkan angka
+let angka = 6
+switch(angka) {
+    case 1: {console.log("Hari Minggu"); break;}
+    case 2: {console.log("Hari Senin"); break;}
+    case 3: {console.log("Hari Selasa"); break;}
+    case 4: {console.log("Hari Rabu"); break;}
+    case 5: {console.log("Hari Kamis"); break;}
+    case 6: {console.log("Hari Jum'at"); break;}
+    case 7: {console.log("Hari Sabtu"); break;}
+}
+console.log("-------------------------------------------")
 
 // TUGAS LOOPING
 
