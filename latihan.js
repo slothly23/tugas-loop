85895ea946224078074d75cb9bd78ef18efcc321
Nur Inayah <150806284+slothly23@users.noreply.github.com>
2023-11-16 20:04:33 +0700

    menambahkan tugas 2

--- latihan.js
+++ latihan.js
@@ -1,5 +1,11 @@
 // menampilkan teks "User ke - 1 ... User ke - 100"
 for (let user = 1; user < 101; user++) {
     console.log("User ke - ", user);
-    
+}
+
+// pengulangan menggunakan FOR LOOP pengulangan 10 kali nilai awal ditambah 2 setiap pengulangan
+let jumlah = 0;
+for (let ulang = 0; ulang < 10; ulang++) {
+    jumlah += 2;
+    console.log(jumlah);
 }
